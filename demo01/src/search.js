e978c62d961bcb71c4507dbb0751008e976c66d9
changxianglin <281532612@qq.com>
2020-01-12 00:51:37 +0800

    parser css and less

--- demo01/src/search.js
+++ demo01/src/search.js
@@ -2,11 +2,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './search.less'
 
 class Search extends React.Component {
 
   render() {
-    return <div>Search Text</div>
+    return <div className="search-text">Search Text</div>
   }
 }
 
