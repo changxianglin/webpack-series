8f6e279d7311779d57dc37d30ddb5eeef8b2bab5
changxianglin <281532612@qq.com>
2020-01-20 00:15:05 +0800

    can use mulit page

--- demo01/webpack.prod.js
+++ demo01/webpack.prod.js
@@ -13,7 +13,29 @@ const setMPA = () => {
 
   const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'))
 
-  console.log('entryFiles', entryFiles)
+  Object.keys(entryFiles)
+    .map((index) => {
+      const entryFile = entryFiles[index]
+
+      const match = entryFile.match(/src\/(.*)\/index\.js/)
+      const pageName = match && match[1]
+
+      entry[pageName] = entryFile
+      htmlWebpackPlugins.push(new HtmlWebpackPlugin({
+        template: path.join(__dirname, `/src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false
+        }
+      }))
+    })
 
   return {
     entry,
@@ -21,13 +43,10 @@ const setMPA = () => {
   }
 }
 
-setMPA()
+const { entry, htmlWebpackPlugins } = setMPA()
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    search: './src/search.js'
-  },
+  entry: entry,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name]_[chunkhash:8].js'
@@ -103,34 +122,6 @@ module.exports = {
       assetNameRegExp: /\.css$/g,
       cssProcessor: require('cssnano')
     }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/src/search.html'),
-      filename: 'search.html',
-      chunks: ['search'],
-      inject: true,
-      minify: {
-        html5: true,
-        collapseWhitespace: true,
-        preserveLineBreaks: false,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/src/index.html'),
-      filename: 'index.html',
-      chunks: ['index'],
-      inject: true,
-      minify: {
-        html5: true,
-        collapseWhitespace: true,
-        preserveLineBreaks: false,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false
-      }
-    }),
     new CleanWebpackPlugin()
-  ]
+  ].concat(htmlWebpackPlugins)
 }
