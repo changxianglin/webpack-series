0dc03996b3db757dbf56ec2ed04e0a26008f512d
changxinaglin <281532612@qq.com>
2020-12-16 18:48:56 +0800

    finish Symbol

--- demo3/src/index.js
+++ demo3/src/index.js
@@ -233,8 +233,206 @@ const log = console.log.bind(console)
 // const y = Symbol.for('bar')
 // log(x === y)
 
-iframe = document.createElement('iframe')
-iframe.src = String(window.location)
-document.body.appendChild(iframe)
+// iframe = document.createElement('iframe')
+// iframe.src = String(window.location)
+// document.body.appendChild(iframe)
 
-log(iframe.contentWindow.Symbol.for('foo') === Symbol.for('foo'))
+// log(iframe.contentWindow.Symbol.for('foo') === Symbol.for('foo'))
+
+
+// function A() {
+//   this.foo = 'hello'
+// }
+
+// if (!global._foo) {
+//   global._foo = new A()
+// }
+
+// modules.exports = global._foo
+
+// class MyClass {
+//   [Symbol.hasInstance(foo)](foo) {
+//     return foo instanceof Array
+//   }
+// }
+
+// log([1, 2, 3] instanceof new MyClass())
+
+// class Even {
+//   static [Symbol.hasInstance](obj) {
+//     return Number(obj) % 2 === 0
+//   }
+// }
+
+// const Even = {
+//   [Symbol.hasInstance](obj) {
+//     return Number(obj) % 2 === 0
+//   }
+// }
+
+// log(1 instanceof Even)
+// log(2 instanceof Even)
+// log(12345 instanceof Even)
+
+
+// let arr1 = ['c', 'd']
+// log(['a', 'b'].concat(arr1, 'e'))
+// log(arr1[Symbol.isConcatSpreadable])
+
+// let arr2 = ['c', 'd']
+// log(arr2[Symbol.isConcatSpreadable] = false)
+// log(['a', 'b'].concat(arr2, 'e'))
+
+// let obj = { length: 2, 0: 'c', 1: 'd' }
+// log(['a', 'b'].concat(obj, 'e'))
+
+// obj[Symbol.isConcatSpreadable] = true
+// log(['a', 'b'].concat(obj, 'e'))
+
+
+// class A1 extends Array {
+//   constructor(args) {
+//     super(args)
+//     this[Symbol.isConcatSpreadable] = true
+//   }
+// }
+
+// class A2 extends Array {
+//   constructor(args) {
+//     super(args)
+//   }
+//   get [Symbol.isConcatSpreadable]() {
+//     return false
+//   }
+// }
+
+// let a1 = new A1()
+// a1[0] = 3
+// a1[1] = 4
+// let a2 = new A2()
+// a2[0] = 5
+// a2[1] = 6
+// log([1, 2].concat(a1).concat(a2))
+
+
+// class MyArray extends Array {
+//   static get [Symbol.species]() { return Array }
+// }
+
+// const a = new MyArray(1, 2, 3)
+// const b = a.map(x => x)
+// const c = a.filter(x => x > 1)
+
+// log(b instanceof MyArray)
+// log(c instanceof MyArray)
+
+// class MyArray extends Array {
+//   static get [Symbol.species]() { return Array }
+// }
+
+// const a = new MyArray()
+// const b = a.map(x => x)
+
+// log(b instanceof MyArray)
+// log(b instanceof Array)
+
+// class T1 extends Promise {
+
+// }
+
+// class T2 extends Promise {
+//   static get [Symbol.species]() {
+//     return Promise
+//   }
+// }
+
+// log(new T1(r => r()).then(v => v) instanceof T1)
+// log(new T2(r => r()).then(v => v) instanceof T2)
+
+// String.prototype.match(regexp)
+
+// regexp[Symbol.match](this)
+
+// class MyMatcher {
+//   [Symbol.match](string) {
+//     return 'hello world'.indexOf(string)
+//   }
+// }
+
+// log('e'.match(new MyMatcher()))
+
+// String.prototype.replace(searchValue, replaceValue)
+// searchValue[Symbol.replace](this, replaceValue)
+
+// const x = {}
+// x[Symbol.replace] = (...s) => console.log(s)
+
+// 'Hello'.replace(x, 'World')
+
+
+// String.prototype.search(regexp)
+// regexp[Symbol.search](this)
+
+// class Mysearch {
+//   constructor(value) {
+//     this.value = value
+//   }
+//   [Symbol.search](string) {
+//     return string.indexOf(this.value)
+//   }
+// }
+
+// log('foobar'.search(new Mysearch('foo')))
+
+// String.prototype.split(separator, limit)
+// separator[Symbol.split](this, limit)
+
+// class MySplitter {
+//   constructor(value) {
+//     this.value = value
+//   }
+
+//   [Symbol.split](string) {
+//     let index = string.indexOf(this.value)
+//     if (index === -1) {
+//       return string
+//     }
+//     return [
+//       string.substr(0, index),
+//       string.substr(index + this.value.length)
+//     ]
+//   }
+// }
+
+// log('foobar'.split(new MySplitter('foo')))
+
+// log('foobar'.split(new MySplitter('bar')))
+
+// log('foobar'.split(new MySplitter('baz')))
+
+// const myIterable = {}
+// myIterable[Symbol.iterator] = function* () {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+// log([...myIterable])
+
+// class Collection {
+//   *[Symbol.iterator]() {
+//     let i = 0
+//     while (this[i] !== undefined) {
+//       yield this[i]
+//       ++i
+//     }
+//   }
+// }
+
+// let myCollection = new Collection()
+// myCollection[0] = 1
+// myCollection[1] = 2
+
+// for (let value of myCollection) {
+//   log(value)
+// }
