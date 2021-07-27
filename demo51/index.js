91072574f15cf1f52a699b01db83252d4dc82063
changxianglin <281532612@qq.com>
2021-07-27 15:42:04 +0800

    finish one

--- demo51/index.js
+++ demo51/index.js
@@ -1,6 +1,6 @@
 const log = console.log.bind(console);
 
-log("work");
+log("parse start");
 
 const TokenType = {
   auto: "AUTO",
@@ -38,27 +38,6 @@ const isDigit = function (c) {
   return digits.indexOf(c) > -1;
 };
 
-class TokenList {
-  constructor(tokens) {
-    this.tokens = tokens;
-    this.index = 0;
-  }
-
-  readToken() {
-    var i = this.index;
-    this.index += 1;
-    if (i < this.tokens.length) {
-      return this.tokens[i];
-    } else {
-      return null;
-    }
-  }
-
-  hasToken() {
-    return this.index < this.tokens.length;
-  }
-}
-
 const isSpace = function (c) {
   var ws = " \t\n";
   return ws.indexOf(c) > -1;
@@ -93,8 +72,7 @@ const readString = function (code) {
     if (c == "'") {
       var s = code.slice(i, index);
 
-      i = index;
-      console.log("read str", s, code[i]);
+      i = index - 1;
       return s;
     }
   }
@@ -102,7 +80,6 @@ const readString = function (code) {
 
 const tokens = function (code) {
   var ts = [];
-  // var i = 0;
   i = 0;
   while (i < code.length) {
     var e = code[i];
@@ -118,7 +95,7 @@ const tokens = function (code) {
       var t = new Token(e, TokenType.string);
       ts.push(t);
     } else if (isSpace(e)) {
-      log("空格");
+      // log("空格");
     } else {
       log("wrong tokens");
     }
@@ -129,29 +106,134 @@ const tokens = function (code) {
   return ts;
 };
 
-const __main = function () {
-  var tests = [
-    `
-    {}
-    `,
-    `
-    []
-    `,
-    `
-    [12 234]
-    `,
-    `
-    {
-      'key' : 123,
-      'value' : 456
-    }
-    `,
-  ];
-
-  for (let e of tests) {
-    var ts = tokens(e);
-    log("这里的问题", ts);
+class TokenList {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.index = 0;
   }
+
+  readToken() {
+    var i = this.index;
+    this.index += 1;
+    if (i < this.tokens.length) {
+      return this.tokens[i];
+    } else {
+      return null;
+    }
+  }
+
+  peekToken() {
+    var i = this.index;
+    if (i < this.tokens.length) {
+      return this.tokens[i];
+    } else {
+      return null;
+    }
+  }
+
+  hasToken() {
+    return this.index < this.tokens.length;
+  }
+}
+
+const parseMap = function (tokenlist) {
+  var tl = tokenlist;
+
+  var m = {};
+  while (tl.hasToken()) {
+    var t1 = tl.readToken();
+    var t2 = tl.readToken();
+    var t3 = tl.readToken();
+
+    if (t3.type == TokenType.braceLeft) {
+      var v = parseMap(tokenlist);
+      m[t1.value] = v;
+    } else {
+      m[t1.value] = t3.value;
+    }
+
+    var t4 = tl.peekToken();
+    if (t4.type == TokenType.comma) {
+      tl.readToken();
+    } else if (t4.type == TokenType.braceRight) {
+      // map 结束
+      break;
+    } else {
+      log("map 报错");
+    }
+  }
+  return m;
 };
 
-__main();
+const parseJson = function (tokenlist) {
+  var tl = tokenlist;
+
+  var l = [];
+  while (tl.hasToken()) {
+    var t = tl.readToken();
+    if (t.type == TokenType.braceLeft) {
+      var t1 = parseMap(tl);
+      l.push(t1);
+    } else if (t.type == TokenType.braceRight) {
+      break;
+    } else {
+      log("wrong type");
+    }
+  }
+  return l;
+};
+
+const parse = function () {
+  // var tests = [
+  //   `
+  //   {}
+  //   `,
+  //   `
+  //   []
+  //   `,
+  //   `
+  //   [12 234]
+  //   `,
+  //   `
+  //   {
+  //     'key' : 123,
+  //     'value' : 456
+  //   }
+  //   `,
+  // ];
+  // for (let e of tests) {
+  //   var ts = tokens(e);
+  //   log("这里的问题", ts);
+  // }
+
+  // var jsonString = `
+  //   {
+  //     'number': 2,
+  //     'v': '1.2'
+  //   }
+  // `;
+
+  var jsonString1 = `
+    {
+      'number': 2,
+      'values': {
+          'name': 'JavaScript',
+          'version': {
+            'name': {
+              'name': 'JavaScript',
+              'version': 6
+            },
+            'version': 1
+          }
+        }
+    }
+  `;
+  var ts = tokens(jsonString1);
+  var tl = new TokenList(ts);
+  var d = parseJson(tl);
+  log("end", d);
+};
+
+// parse();
+
+log("htmlparse start");
