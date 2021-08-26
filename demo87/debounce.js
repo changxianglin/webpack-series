6d684577fa0b05be68f0e0bcee56eaacb2a9b660
changxianglin <281532612@qq.com>
2021-08-26 20:42:30 +0800

    use transform

--- demo87/debounce.js
+++ demo87/debounce.js
@@ -23,21 +23,97 @@ const getUserAction = function () {
 // }
 
 // 2 版
-function throttle(func, wait) {
-  var timeout;
-  var previous = 0;
+// function throttle(func, wait) {
+//   var timeout;
+//   var previous = 0;
 
-  return function () {
+//   return function () {
+//     context = this;
+//     args = arguments;
+
+//     if (!timeout) {
+//       timeout = setTimeout(() => {
+//         timeout = null;
+//         func.apply(context, args);
+//       }, wait);
+//     }
+//   };
+// }
+
+// 3 版
+// function throttle(func, wait) {
+//   var timeout, context, args, result;
+//   var previous = 0;
+
+//   var later = function () {
+//     previous = +new Date();
+//     timeout = null;
+//     func.apply(context, args);
+//   };
+
+//   var throttled = function () {
+//     var now = +new Date();
+
+//     var remaining = wait - (now - previous);
+//     context = this;
+//     args = arguments;
+
+//     if (remaining <= 0 || remaining > wait) {
+//       if (timeout) {
+//         clearTimeout(timeout);
+//         timeout = null;
+//       }
+//       previous = now;
+//       func.apply(context, args);
+//     } else if (!timeout) {
+//       timeout = setTimeout(later, remaining);
+//     }
+//   };
+
+//   return throttled;
+// }
+
+// 4 版
+
+function throttle(func, wait, options) {
+  var timeout, context, args, result;
+  var previous = 0;
+  if (!options) options = {};
+
+  var later = function () {
+    previous = options.leading === false ? 0 : new Date().getTime();
+    timeout = null;
+    func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+
+  var throttled = function () {
+    var now = new Date().getTime();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
     context = this;
     args = arguments;
 
-    if (!timeout) {
-      timeout = setTimeout(() => {
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
         timeout = null;
-        func.apply(context, args);
-      }, wait);
+      }
+      previous = now;
+      func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeOut(later, remaining);
     }
   };
+
+  throttled.cacel = function () {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = null;
+  };
+
+  return throttled;
 }
 
 container.onmousemove = throttle(getUserAction, 1000);
